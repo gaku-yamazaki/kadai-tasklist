47ea0cf204d5a3c2a6993da73c33ca6c54e91102
gaku.yamazaki <gaku.ymzk6@gmail.com>
2018-05-27 06:07:33 +0000

    for heroku

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ Bundler.require(*Rails.groups)
 
 module Tasklist
   class Application < Rails::Application
+    ActiveRecord::Base.protected_environments = %w(production staging)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
